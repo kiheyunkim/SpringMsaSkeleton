06ba31ef74d9f4dd958b2d727fde2442e08d54b9
kiheyunkim <kiheyunkim@gmail.com>
2022-02-13 12:33:38 +0900

    eureka 설정 및 api-gateway 예시 대응 exception throw

--- src/main/kotlin/com/kihyeonkim/springmsaskeleton/user/controller/UserController.kt
+++ src/main/kotlin/com/kihyeonkim/springmsaskeleton/user/controller/UserController.kt
@@ -2,16 +2,27 @@ package com.kihyeonkim.springmsaskeleton.user.controller
 
 import com.kihyeonkim.springmsaskeleton.user.model.UserModel
 import com.kihyeonkim.springmsaskeleton.user.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
 
 @Controller
+@ResponseBody
 @RequestMapping("/user")
 class UserController(private val userService: UserService) {
 
 	@GetMapping("/getInfo")
-	fun getUserInfo(userId: String): UserModel {
+	fun getUserInfo(@RequestParam userId: String): UserModel {
 		return userService.getUserInfo(userId)
+	}
+
+	@GetMapping("/getInfo2")
+	fun getUserInfo2(): ResponseEntity<Object> {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
 	}
 }
